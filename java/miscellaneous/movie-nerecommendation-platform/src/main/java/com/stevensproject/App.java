83f8fa7e1682e09012b573b9ef4c9e9e60291c02
petreastefann <petreastefann@gmail.com>
2022-12-21 01:29:14 +0200

    updated: insertIntoTableByFile and createTableMovies functions for jdbc

--- java/miscellaneous/movie-nerecommendation-platform/src/main/java/com/stevensproject/App.java
+++ java/miscellaneous/movie-nerecommendation-platform/src/main/java/com/stevensproject/App.java
@@ -1,13 +1,15 @@
 package com.stevensproject;
 
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement; // https://www.baeldung.com/java-statement-preparedstatement
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Date;
 
 public class App {
 	public Connection connect() {
@@ -24,59 +26,81 @@ public class App {
 			System.out.println("Connected to the PostgreSQL server successfully.");
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
+			System.exit(1);
 		}
 
 		console.close();
-
 		return connection;
 	}
 
 	public static void createTableMovies(Connection connection) throws SQLException {
-		String queryCreateTable = "create table if not exists movies(movie_name varchar(40) not null, genre varchar(20) not null, release_date date not null, likes int not null);";
+		String queryCreateTable = "SELECT to_regclass('public.movies');";
 		PreparedStatement stmt = connection.prepareStatement(queryCreateTable);
-		stmt.executeUpdate();
-		// -maybe put try catch maybe??
-		// -and maybe surely put queryOutput for table so if it exists then print "table
-		// already exists" and if not "table created successfully"??
+		ResultSet queryOutput = stmt.executeQuery();
+		queryOutput.next();
+		if (queryOutput.getString(1) == null) {
+			queryCreateTable = "create table if not exists movies(movie_name varchar(40) not null, genre varchar(20) not null, release_date date not null, likes int not null);";
+			stmt = connection.prepareStatement(queryCreateTable);
+			stmt.executeUpdate();
+			System.out.println("table \"movies\" created successfully");
+		} else {
+			System.out.println("the table \"movies\" already exists");
+		}
+
 		stmt.close();
-		System.out.println("table created successfully");
 	}
 
-	public static void insertIntoTable(Connection connection) {
+	public static void insertIntoTableByConsole(Connection connection) {
 		PreparedStatement stmt = null;
 		ResultSet queryOutput = null;
-		String queryInsert = "INSERT INTO table1 (movie_name, genre, release_date, likes) VALUES (?, ?, ?, ?);";
-		String queryCount = "select count(*) from table1 where movie_name = 'Adventure Time';";
+		String queryInsert = "";
+	}
+	// finish it
+
+	public static void insertIntoTableByFile(Connection connection) {
+		PreparedStatement stmt = null;
+		ResultSet queryOutput = null;
+		String queryInsert = "insert into movies (movie_name, genre, release_date, likes) values (?, ?, ?, ?);";
+		String queryCount1 = "select count(*) from movies where movie_name = ?;"; // + "'movie_name';"
+
 		try {
-			stmt = connection.prepareStatement(queryCount);
-			queryOutput = stmt.executeQuery();
-			queryOutput.next();
-			System.out.println(queryOutput.getInt(1));
-			if (queryOutput.getInt(1) == 0) { // idk why but i have to make this reversed (it shouldve been '!= 0')
-				stmt = connection.prepareStatement(queryInsert);
-				stmt.setString(1, "Adventure Time");
-				stmt.setString(2, "Cartoon");
-				stmt.setDate(3, Date.valueOf("2010-04-05"));
-				stmt.setInt(4, 100);
-				stmt.executeUpdate();
-			} else {
-				System.out.println("Movie already exists in the table");
+			File movieList = new File("movie_list.in");
+			Scanner fileReader = new Scanner(movieList);
+			String line = fileReader.nextLine(); // ignore the first line (it contains the name of each column)
+
+			while (fileReader.hasNextLine()) {
+				line = fileReader.nextLine();
+				String[] tokens = line.split(", ");
+
+				stmt = connection.prepareStatement(queryCount1);
+				stmt.setString(1, tokens[0]);
+				queryOutput = stmt.executeQuery();
+				queryOutput.next();
+
+				if (queryOutput.getInt(1) == 0) {
+					stmt = connection.prepareStatement(queryInsert);
+					stmt.setString(1, tokens[0]);
+					stmt.setString(2, tokens[1]);
+					stmt.setDate(3, Date.valueOf(tokens[2]));
+					stmt.setInt(4, Integer.parseInt(tokens[3]));
+					stmt.executeUpdate();
+				} else {
+					System.out.println("the movie \"" + tokens[0] + "\" already exists in the table");
+				}
+
+				// connection.commit(); --> org.postgresql.util.PSQLException: Cannot commit when autoCommit is enabled.
 			}
 
-			// stmt.setString(1, "GOT");
-			// stmt.setString(2, "Fantasy");
-			// stmt.setDate(3, Date.valueOf("2011-04-17"));
-			// stmt.setInt(4, 80);
-			// stmt.executeUpdate();
-
-			// connection.commit(); --> org.postgresql.util.PSQLException: Cannot commit
-			// when autoCommit is enabled.
-			queryOutput.close();
 			stmt.close();
-		} catch (Exception e) {
+			queryOutput.close();
+			fileReader.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("The file could not be found...");
+			e.printStackTrace();
+		} catch (SQLException e) {
 			System.err.println(e.getClass().getName() + ": " + e.getMessage());
-			System.exit(1);
 		}
+
 		System.out.println("Records created successfully");
 	}
 
@@ -84,7 +108,9 @@ public class App {
 		App app = new App();
 		Connection connection = app.connect();
 		createTableMovies(connection);
-		// insertIntoTable(connection);
+		insertIntoTableByFile(connection);
+		// make a menu
+
 		connection.close();
 	}
 }
