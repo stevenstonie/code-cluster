44aef7622f36ba9a196bb94ea1cdef1551056873
petreastefann <petreastefann@gmail.com>
2023-03-09 18:12:55 +0200

    added: new images and a new solution for placing images on a matrix of buttons

--- c#/memoryGame/TestWindow.xaml.cs
+++ c#/memoryGame/TestWindow.xaml.cs
@@ -10,6 +10,46 @@ namespace memoryGame {
 		public TestWindow() {
 			InitializeComponent();
 
+			// Define the dimensions of the matrix
+			int cols = 3;
+			int rows = 4;
+
+			// Create a Grid to hold the buttons
+			Grid grid = new Grid();
+
+			// Create the rows of the grid
+			for(int i = 0; i < rows; i++) {
+				RowDefinition rowDef = new RowDefinition();
+				grid.RowDefinitions.Add(rowDef);
+			}
+
+			// Create the columns of the grid
+			for(int i = 0; i < cols; i++) {
+				ColumnDefinition colDef = new ColumnDefinition();
+				grid.ColumnDefinitions.Add(colDef);
+			}
+
+			// Create the buttons and add them to the grid
+			for(int i = 0; i < rows; i++) {
+				for(int j = 0; j < cols; j++) {
+					Button button = new Button();
+					button.Content = string.Format("Button {0},{1}", i, j);
+					button.Height = 50;
+					button.Width = 50;
+					Grid.SetRow(button, i);
+					Grid.SetColumn(button, j);
+					grid.Children.Add(button);
+				}
+			}
+
+			// Add some margin to the right of the grid
+			grid.Margin = new Thickness(0, 0, 50, 0);
+
+			// Add the grid to your WPF window
+			this.Content = grid;
+
+			//////
+
 			// specify the folder path where your images are stored
 			string folderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Assets", "GridImages");
 
@@ -22,111 +62,24 @@ namespace memoryGame {
 			};
 
 			// get the file names of all the images in the folder
-			string[] fileNames = Directory.GetFiles(folderPath, "*.png");
+			string[] images = Directory.GetFiles(folderPath, "*.png");
 
-			// loop through the file names and create an Image control for each one
-			int row = 0;
-			int column = 0;
-			foreach(string fileName in fileNames) {
-				// create a new Image control and set its properties
-				var image = new Image {
-					Source = new BitmapImage(new Uri(fileName)),
-					Width = 100,
-					Height = 100,
-					Margin = new Thickness(5),
-					Stretch = Stretch.Fill
-				};
+			// Create the buttons and add them to the grid
+			for(int i = 0; i < rows; i++) {
+				for(int j = 0; j < cols; j++) {
+					Button button = new Button();
 
-				// add the Image control to the WrapPanel
-				wrapPanel.Children.Add(image);
+					// Set the background of the button to an image
+					ImageBrush imageBrush = new ImageBrush();
+					imageBrush.ImageSource = new BitmapImage(new Uri(images[i * cols + j], UriKind.Relative));
+					imageBrush.Stretch = Stretch.Uniform;
+					button.Background = imageBrush;
 
-				// increment the row and column counters
-				column++;
-				if(column > 2) {
-					row++;
-					column = 0;
+					Grid.SetRow(button, i);
+					Grid.SetColumn(button, j);
+					grid.Children.Add(button);
 				}
 			}
-
-			// add the WrapPanel to the grid
-			Grid.SetRow(wrapPanel, 0);
-			Grid.SetColumn(wrapPanel, 0);
-			ImageGrid.Children.Add(wrapPanel);
 		}
 	}
-	/*
-		public partial class TestWindow : Window {
-		public ObservableCollection<string> StringItems {
-			get; set;
-		}
-
-		public TestWindow() {
-			InitializeComponent();
-
-			// initialize the StringItems collection
-			StringItems = new ObservableCollection<string> { "something", "else", "Button 3", "Button 4", "Button 5" };
-
-			// set the data context of the window to this instance
-			DataContext = this;
-
-			// specify the folder path where your images are stored
-			string folderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Assets", "GridImages");
-
-			// create a new list to store the image paths
-			List<string> imagePaths = new List<string>();
-
-			// get the file names of all the images in the folder and add them to the list
-			string[] fileNames = Directory.GetFiles(folderPath, "*.png");
-			foreach(string fileName in fileNames) {
-				var image = new Image {
-					Source = new BitmapImage(new Uri(fileName)),
-					Width = 100,
-					Height = 100,
-					Margin = new Thickness(5),
-					Stretch = Stretch.Uniform
-				};
-
-				ImageGrid.Children.Add(image);
-			}
-
-
-			// set the data context of the window to the list of image paths
-			DataContext = imagePaths;
-
-
-			// specify the folder path where your images are stored
-			string folderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Assets", "GridImages");
-
-			// create a new WrapPanel and set its properties
-			var wrapPanel = new WrapPanel {
-				Orientation = Orientation.Horizontal,
-				HorizontalAlignment = HorizontalAlignment.Center,
-				VerticalAlignment = VerticalAlignment.Center,
-				Margin = new Thickness(10)
-			};
-
-			// get the file names of all the images in the folder
-			string[] fileNames = Directory.GetFiles(folderPath, "*.png");
-
-			// loop through the file names and create an Image control for each one
-			foreach(string fileName in fileNames) {
-				// create a new Image control and set its properties
-				var image = new Image {
-					Source = new BitmapImage(new Uri(fileName)),
-					Width = 100,
-					Height = 100,
-					Margin = new Thickness(5)
-				};
-
-				// add the Image control to the WrapPanel
-				wrapPanel.Children.Add(image);
-			}
-
-			// add the WrapPanel to the grid
-			Grid.SetRow(wrapPanel, 0);
-			Grid.SetColumn(wrapPanel, 0);
-			ImageGrid.Children.Add(wrapPanel);
-}
-	}
-*/
 }
