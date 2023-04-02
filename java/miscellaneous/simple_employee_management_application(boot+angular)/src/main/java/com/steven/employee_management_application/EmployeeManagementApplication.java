43aa570f6a6284bfd9b4216cf2e0872668f33d17
petreastefann <petreastefann@gmail.com>
2023-04-02 16:12:51 +0300

    added: new proj tutorial for a simple employee management application

---
+++ java/miscellaneous/simple_employee_management_application(boot+angular)/src/main/java/com/steven/employee_management_application/EmployeeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.steven.employee_management_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementApplication.class, args);
+	}
+
+}
