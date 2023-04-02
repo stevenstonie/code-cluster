ef0f9f65a00d7a9e2637e0c5eeed6a7f3607634d
petreastefann <petreastefann@gmail.com>
2023-04-02 12:42:14 +0300

    added: student update functionality and deleted hardcoded entries in main

--- java/miscellaneous/student_management_system/src/main/java/com/steven/student_management_system/controller/StudentController.java
+++ java/miscellaneous/student_management_system/src/main/java/com/steven/student_management_system/controller/StudentController.java
@@ -4,7 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import com.steven.student_management_system.entity.Student;
 import com.steven.student_management_system.service.StudentService;
@@ -34,6 +36,25 @@ public class StudentController {
 	@PostMapping("/students")
 	public String addStudent(@ModelAttribute("student") Student student) {
 		studentService.saveStudent(student);
+		return "redirect:/students";
+	}
+
+	@GetMapping("/students/edit/{id}")
+	public String editStudentForm(@PathVariable Long id, Model model) {
+		model.addAttribute("student", studentService.getStudentById(id));
+		return "edit_student";
+	}
+
+	@PostMapping("/students/{id}")
+	public String updateStudent(@PathVariable Long id, @ModelAttribute("student") Student student, Model model) {
+		Student existingStudent = studentService.getStudentById(id);
+
+		existingStudent.setId(student.getId());
+		existingStudent.setFirstName(student.getFirstName());
+		existingStudent.setLastName(student.getLastName());
+		existingStudent.setEmail(student.getEmail());
+
+		studentService.updateStudent(existingStudent);
 		return "redirect:/students";
 	}
 }
