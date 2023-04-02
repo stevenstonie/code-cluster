34dae73691eb3f33906c170aadf54dbad1c7a259
petreastefann <petreastefann@gmail.com>
2023-04-02 15:25:49 +0300

    added request mapping on class to wrap all paths with /students

--- java/miscellaneous/student_management_system/src/main/java/com/steven/student_management_system/controller/StudentController.java
+++ java/miscellaneous/student_management_system/src/main/java/com/steven/student_management_system/controller/StudentController.java
@@ -6,11 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.steven.student_management_system.entity.Student;
 import com.steven.student_management_system.service.StudentService;
 
 @Controller
+@RequestMapping("/students")
 public class StudentController {
 
 	private StudentService studentService;
@@ -20,35 +22,35 @@ public class StudentController {
 	}
 
 	// handles HTTP GET requests to '/students' and returns a list of all the students
-	@GetMapping("/students")
+	@GetMapping("")
 	public String listStudents(Model model) {
 		model.addAttribute("students", studentService.getAllStudents());
 		return "students";
 	}
 
 	// handles GET requests to '/students/add' which returns a form to add a new student
-	@GetMapping("/students/add")
+	@GetMapping("/add")
 	public String addStudentForm(Model model) {
 		model.addAttribute("student", new Student());
 		return "add_student";
 	}
 
 	// handles the submission of the form to add a new student via a POST request to '/students'
-	@PostMapping("/students")
+	@PostMapping("")
 	public String addStudent(@ModelAttribute("student") Student student) {
 		studentService.saveStudent(student);
 		return "redirect:/students";
 	}
 
 	// handles GET requests to '/students/edit/{id}' which returns a form to edit an existing student
-	@GetMapping("/students/edit/{id}")
+	@GetMapping("/edit/{id}")
 	public String editStudentForm(@PathVariable Long id, Model model) {
 		model.addAttribute("student", studentService.getStudentById(id));
 		return "edit_student";
 	}
 
 	// handles the submission of the form to edit a student via a POST request to '/students/{id}'
-	@PostMapping("/students/{id}")
+	@PostMapping("/{id}")
 	public String updateStudent(@PathVariable Long id, @ModelAttribute("student") Student student, Model model) {
 		Student existingStudent = studentService.getStudentById(id);
 
@@ -59,7 +61,7 @@ public class StudentController {
 	}
 
 	// handles GET requests to '/students/{id}' to delete an existing student
-	@GetMapping("/students/{id}")
+	@GetMapping("/{id}")
 	public String deleteStudent(@PathVariable Long id) {
 		studentService.deleteStudentById(id);
 		return "redirect:/students";
