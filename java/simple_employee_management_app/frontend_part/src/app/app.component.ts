842318606ab29e8d8bec15ca30586be6565be8de
petreastefann <petreastefann@gmail.com>
2023-04-03 23:16:38 +0300

    connected angular with create employee rest api

--- java/simple_employee_management_app/frontend_part/src/app/app.component.ts
+++ java/simple_employee_management_app/frontend_part/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontend_part';
+  title = 'Employee Management System with Angular and Spring Boot';
 }
