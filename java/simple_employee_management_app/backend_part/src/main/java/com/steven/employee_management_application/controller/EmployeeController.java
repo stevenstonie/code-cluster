addd76fed2b928ed4e1dc9f2435b0e4bd34ae80a
petreastefann <petreastefann@gmail.com>
2023-04-03 22:41:27 +0300

    added: two way routing and get method in controller class

--- java/simple_employee_management_app/backend_part/src/main/java/com/steven/employee_management_application/controller/EmployeeController.java
+++ java/simple_employee_management_app/backend_part/src/main/java/com/steven/employee_management_application/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +25,10 @@ public class EmployeeController {
 	@GetMapping("/employees")
 	public List<Employee> getAllEmployees() {
 		return employeeRepository.findAll();
+	}
+
+	@PostMapping("/employees")
+	public Employee createEmployee(@RequestBody Employee employee) {
+		return employeeRepository.save(employee);
 	}
 }
--- java/simple_employee_management_app/frontend_part/src/app/app-routing.module.ts
+++ java/simple_employee_management_app/frontend_part/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
 
 const routes: Routes = [
   { path: 'employees', component: EmployeeListComponent },
+  { path: 'create-employee', component: CreateEmployeeComponent },
   { path: '', redirectTo: 'employees', pathMatch: 'full' }
 ];
 
