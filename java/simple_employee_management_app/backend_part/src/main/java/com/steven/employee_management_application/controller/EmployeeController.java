11c7709ed1d4c840c1b90e5b710e2aeda8b864fd
petreastefann <petreastefann@gmail.com>
2023-04-05 11:00:30 +0300

    added: functionality of deleting an user in controller

--- java/simple_employee_management_app/backend_part/src/main/java/com/steven/employee_management_application/controller/EmployeeController.java
+++ java/simple_employee_management_app/backend_part/src/main/java/com/steven/employee_management_application/controller/EmployeeController.java
@@ -1,10 +1,12 @@
 package com.steven.employee_management_application.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,21 +40,36 @@ public class EmployeeController {
 
 	@GetMapping("/employees/{id}")
 	public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) { // @PathVariable is used to get the id from the URL
-		Employee employee = employeeRepository.findById(id)
-				.orElseThrow(() -> new ResourceNotFoundException("Employee not found with id: " + id));
+		Employee employee = _getEmployeeById(id);
 
 		return ResponseEntity.ok(employee);
 	}
 
 	@PutMapping("/employees/{id}")
 	public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeNewDetails) {
-		Employee employee = employeeRepository.findById(id)
-				.orElseThrow(() -> new ResourceNotFoundException("Employee with id : " + id + " does not exist."));
+		Employee employee = _getEmployeeById(id);
 
 		setCredentialsOfEmployee(employee, employeeNewDetails);
 
 		Employee updatedEmployee = employeeRepository.save(employee);
 		return ResponseEntity.ok(updatedEmployee);
+	}
+
+	@DeleteMapping("/employees/{id}")
+	public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable Long id) {
+		Employee employee = _getEmployeeById(id);
+
+		employeeRepository.delete(employee);
+
+		Map<String, Boolean> response = Map.of("deleted", Boolean.TRUE);
+		return ResponseEntity.ok(response);
+	}
+
+	//
+
+	private Employee _getEmployeeById(Long id) {
+		return employeeRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee with id : " + id + " does not exist."));
 	}
 
 	private void setCredentialsOfEmployee(Employee employee, Employee employeeNewDetails) {
