959dbd58093d6990847c1aec746be854307ff91f
petreastefann <petreastefann@gmail.com>
2023-04-10 20:48:10 +0300

    added: project showcasing a real world example of huge datastreams being handled by kafka

---
+++ java/kafka_in_springboot/wikimedia_to_db-proj/src/main/java/com/steven/app/WikimediaToDbProjApplication.java
@@ -0,0 +1,13 @@
+package com.steven.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikimediaToDbProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikimediaToDbProjApplication.class, args);
+	}
+
+}
