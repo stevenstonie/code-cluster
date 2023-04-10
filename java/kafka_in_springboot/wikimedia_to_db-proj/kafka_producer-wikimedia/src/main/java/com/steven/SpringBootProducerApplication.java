d9cfe60c0e76328dd6f167e2a68e38286c85a4a2
petreastefann <petreastefann@gmail.com>
2023-04-10 21:57:21 +0300

    configured: commandlinerunner in main for getting the real time data from the producer into the console

--- java/kafka_in_springboot/wikimedia_to_db-proj/kafka_producer-wikimedia/src/main/java/com/steven/SpringBootProducerApplication.java
+++ java/kafka_in_springboot/wikimedia_to_db-proj/kafka_producer-wikimedia/src/main/java/com/steven/SpringBootProducerApplication.java
@@ -1,11 +1,21 @@
 package com.steven;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootProducerApplication {
+public class SpringBootProducerApplication implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootProducerApplication.class);
+    }
+
+    @Autowired
+    private WikimediaChangesProducer wikimediaChangesProducer;
+
+    @Override
+    public void run(String... args) throws Exception {
+        wikimediaChangesProducer.sendMessage();
     }
 }
