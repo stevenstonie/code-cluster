b18895f58da0b4b612a4d5736cd9c73c9e4704f1
petreastefann <petreastefann@gmail.com>
2023-04-10 21:08:54 +0300

    added: a new module to the wikimedia_to_database project

---
+++ java/kafka_in_springboot/wikimedia_to_db-proj/kafka_producer-wikimedia/src/main/java/com/steven/SpringBootProducerApplication.java
@@ -0,0 +1,11 @@
+package com.steven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProducerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootProducerApplication.class);
+    }
+}
