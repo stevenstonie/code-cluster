0fd52cd203c204427ca286bcaf3c57b2f9376bfd
petreastefann <petreastefann@gmail.com>
2023-09-28 18:06:46 +0300

    added posibility of getting the games list in the main menu

--- frontend/angular/video-game-online-db/src/app/components/home/home.component.ts
+++ frontend/angular/video-game-online-db/src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { APIResponse, Game } from 'src/app/models';
+import { HttpService } from 'src/app/services/http.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +9,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-  public sort!: string;
+  public sort?: string;
+  public games?: Array<Game>;
+
+  constructor(
+    private httpService: HttpService,
+    private activatedRoute: ActivatedRoute) {
+
+  }
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params: Params) => {
+      if (params['game-search']) {
+        this.searchGames('metacrit', params['game-search']);
+      } else {
+        this.searchGames('metacrit');
+      }
+    });
+  }
+
+  searchGames(sort: string, search?: string): void {
+    this.httpService.getGameList(sort, search).subscribe((gameList: APIResponse<Game>) => {
+      this.games = gameList.results;
+      console.log(gameList);
+    });
+  }
 }
