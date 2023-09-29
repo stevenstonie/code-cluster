3f18e3bfb275e6aff3028f9d16a305269be5138d
petreastefann <petreastefann@gmail.com>
2023-09-29 13:46:49 +0300

    added game tabs for more game details

---
+++ frontend/angular/video-game-online-db/src/app/components/game-tabs/game-tabs.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GameTabsComponent } from './game-tabs.component';
+
+describe('GameTabsComponent', () => {
+  let component: GameTabsComponent;
+  let fixture: ComponentFixture<GameTabsComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [GameTabsComponent]
+    });
+    fixture = TestBed.createComponent(GameTabsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/angular/video-game-online-db/src/app/components/game-tabs/game-tabs.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-tabs',
+  templateUrl: './game-tabs.component.html',
+  styleUrls: ['./game-tabs.component.scss']
+})
+export class GameTabsComponent {
+
+}
