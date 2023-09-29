740cd3628afd0a783dc92195131a6dc6e0e070b3
petreastefann <petreastefann@gmail.com>
2023-09-29 13:34:03 +0300

    added code to get details of a game from the site but it doesnt work for now

--- frontend/angular/video-game-online-db/src/app/components/details/details.component.ts
+++ frontend/angular/video-game-online-db/src/app/components/details/details.component.ts
@@ -1,15 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Game } from 'src/app/models';
+import { HttpService } from 'src/app/services/http.service';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit, OnDestroy {
   gameRating = 0;
+  gameId?: string;
+  game?: Game;
+  routeSub?: Subscription;
+  gameSub?: Subscription;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private httpService: HttpService
+  ) { }
+
+  ngOnInit(): void {
+    this.routeSub = this.activatedRoute.params.subscribe((params: Params) => {
+      this.gameId = params['id'];
+      this.getGameDetails(this.gameId!);
+    });
+  }
+
+  getGameDetails(id: string): void {
+    this.gameSub = this.httpService
+      .getGameDetails(id)
+      .subscribe((gameResp: Game) => {
+        this.game = gameResp;
+
+        setTimeout(() => {
+          if (this.game) {
+            this.gameRating = this.game.metacritic;
+          }
+        }, 1000);
+      });
+  }
 
   getColor(value: number): string {
-    if (value > 80) {
+    if (value > 75) {
       return '#5ee432';
     } else if (value > 50) {
       return '#fffa50';
@@ -19,4 +53,15 @@ export class DetailsComponent {
       return '#ef4655';
     }
   }
+
+  ngOnDestroy(): void {
+    if (this.gameSub) {
+      this.gameSub.unsubscribe();
+    }
+
+    if (this.routeSub) {
+      this.routeSub.unsubscribe();
+    }
+  }
+
 }
--- frontend/angular/video-game-online-db/src/app/services/http.service.ts
+++ frontend/angular/video-game-online-db/src/app/services/http.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { environment as env } from 'src/environments/environment';
+import { forkJoin, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment as env } from '../../environments/environment.development';
 import { APIResponse, Game } from '../models';
 
 @Injectable({
@@ -11,7 +12,10 @@ export class HttpService {
 
   constructor(private http: HttpClient) { }
 
-  getGameList(ordering: string, search?: string): Observable<APIResponse<Game>> {
+  getGameList(
+    ordering: string,
+    search?: string
+  ): Observable<APIResponse<Game>> {
     let params = new HttpParams().set('ordering', ordering);
 
     if (search) {
@@ -21,5 +25,29 @@ export class HttpService {
     return this.http.get<APIResponse<Game>>(`${env.BASE_URL}/games`, {
       params: params,
     });
+  }
+
+  getGameDetails(id: string): Observable<Game> {
+    const gameInfoRequest = this.http.get(`${env.BASE_URL}/games/${id}`);
+    const gameTrailersRequest = this.http.get(
+      `${env.BASE_URL}/games/${id}/movies`
+    );
+    const gameScreenshotsRequest = this.http.get(
+      `${env.BASE_URL}/games/${id}/screenshots`
+    );
+
+    return forkJoin({
+      gameInfoRequest,
+      gameScreenshotsRequest,
+      gameTrailersRequest,
+    }).pipe(
+      map((resp: any) => {
+        return {
+          ...resp['gameInfoRequest'],
+          screenshots: resp['gameScreenshotsRequest']?.results,
+          trailers: resp['gameTrailersRequest']?.results,
+        };
+      })
+    );
   }
 }
