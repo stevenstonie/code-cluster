929af284b149bba1d33fc525c39aa20525151a5d
petreastefann <petreastefann@gmail.com>
2023-09-29 10:13:23 +0300

    'hid' the hardcoded api keys

--- frontend/angular/video-game-online-db/src/app/interceptors/http-headers.interceptor.ts
+++ frontend/angular/video-game-online-db/src/app/interceptors/http-headers.interceptor.ts
@@ -1,6 +1,7 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
+import { environment as env } from 'src/environments/environment.development';
 
 @Injectable()
 export class HttpHeadersInterceptor implements HttpInterceptor {
@@ -10,13 +11,13 @@ export class HttpHeadersInterceptor implements HttpInterceptor {
 	): Observable<HttpEvent<any>> {
 		req = req.clone({
 			setHeaders: {
-				'x-rapidapi-key': 'esGbwrm390mshS2BCl0RALxQRtZTp1W7sFMjsnyJlJzDXVkW0H',
-				'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
+				'x-rapidapi-key': env.rapidApiKey,
+				'x-rapidapi-host': env.rapidApiHost,
 			},
 			setParams: {
-				key: 'e40e743af2c94b0c916a8aa618fb4473',
-			}
-		})
+				key: env.apiKey,
+			},
+		});
 		return next.handle(req);
 	}
 }
--- frontend/angular/video-game-online-db/src/environments/environment.development.ts
+++ frontend/angular/video-game-online-db/src/environments/environment.development.ts
@@ -1,4 +1,7 @@
 export const environment = {
 	production: true,
-	BASE_URL: 'https://rawg-video-games-database.p.rapidapi.com'
-};
+	BASE_URL: 'https://rawg-video-games-database.p.rapidapi.com',
+	rapidApiKey: 'esGbwrm390mshS2BCl0RALxQRtZTp1W7sFMjsnyJlJzDXVkW0H',
+	rapidApiHost: 'rawg-video-games-database.p.rapidapi.com',
+	apiKey: 'e40e743af2c94b0c916a8aa618fb4473',
+  };
