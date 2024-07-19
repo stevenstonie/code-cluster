7267e2b088d223f6dfaae1dc11531e8788219061
petreastefann <petreastefann@gmail.com>
2024-07-19 19:22:54 +0300

    made the player face the direction of its movement; added a text at the beginning of the level; + some other small changes

--- game dev/godot-tutorial-from-brackeys/scripts/player.gd
+++ game dev/godot-tutorial-from-brackeys/scripts/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 120.0
 const JUMP_VELOCITY = -300.0
-
+@onready var animatedSprite = $AnimatedSprite2D
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -12,14 +12,27 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("move_left", "move_right")
+	
+	if direction > 0:
+		animatedSprite.flip_h = false;
+	elif direction < 0:
+		animatedSprite.flip_h = true;
+		
+	if is_on_floor():
+		if direction == 0:
+			animatedSprite.play("idle");
+		else:
+			animatedSprite.play("run");
+	else:
+		animatedSprite.play("jump");
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
